d6f978102e74401213531949aa1015415afd38ae
MyniwGuels <166999994+MyniwGuels@users.noreply.github.com>
2025-06-22 20:54:31 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,20 +100,15 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks (however, FPS bypass is allowed)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Recording has to have clicks/taps if the level is above hard demon difficulty. Edited audio only does not count
                     </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
+                    
                     <p>
                         Do not use secret routes or bug routes
                     </p>
@@ -121,7 +116,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                         PLEASE UPLOAD YOUR VIDEO ON YOUTUBE!!!
                     </p>
                 </div>
             </div>
